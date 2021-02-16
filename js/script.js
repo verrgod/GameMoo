0c9a8ee5e2f29052730a224b11ff0b3d9cd5228c
verrgod <s10206142@connect.np.edu.sg>
2021-02-16 15:31:33 +0800

    Add modal form

--- js/script.js
+++ js/script.js
@@ -6,6 +6,25 @@ menu.addEventListener('click',function() {
     menuLinks.classList.toggle('active');
 })
 
+const modal = document.getElementById('email-modal');
+const openBtn = document.querySelector('.main-btn');
+const closeBtn = document.querySelector('.close-btn');
+
+//Click event
+openBtn.addEventListener('click', () => {
+    modal.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+})
+
+window.addEventListener('click', (e) => {
+    if (e.target === modal){
+        modal.style.display = 'none';
+    }
+})
+
 //Check page name
 var path = window.location.pathname;
 var page = path.split("/").pop();
--- js/trivia.js
+++ js/trivia.js
@@ -2,12 +2,15 @@ const mainButton = document.querySelector('.main-btn');
 
 mainButton.addEventListener('click',function() {
     fetch('https://opentdb.com/api.php?amount=10&category=15&difficulty=easy')
-    .then(response => {
-        return response.json();
-    })
-    .then(results => {
-        console.log(results);
-    })
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(results) {
+            console.log(results);
+            console.log(results[0].question)
+        })
 })
+
+
 
  
