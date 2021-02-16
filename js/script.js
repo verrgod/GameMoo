7c517138071bd8b3a026d39eca87ab3fd8991484
Mxrcxsz <s10208495@connect.np.edu.sg>
2021-02-16 23:33:21 +0800

    Added Flappy Bird Game

--- js/script.js
+++ js/script.js
@@ -31,7 +31,8 @@ if(page == "index.html"){
     })
 }
 
-const ticTicToe = '<div class="game-container"><h1>Tic Tac Toe</h1><div class="game--container"><div data-cell-index="0" class="cell"></div><div data-cell-index="1" class="cell"></div><div data-cell-index="2" class="cell"></div><div data-cell-index="3" class="cell"></div><div data-cell-index="4" class="cell"></div><div data-cell-index="5" class="cell"></div><div data-cell-index="6" class="cell"></div><div data-cell-index="7" class="cell"></div><div data-cell-index="8" class="cell"></div></div><h2 class="game--status"></h2><button class="game--restart">Restart Game</button></div>'
+// Start of TicTacToe Code
+const ticTicToe = '<div id="game-container"><h1>Tic Tac Toe</h1><div class="game--container"><div data-cell-index="0" class="cell"></div><div data-cell-index="1" class="cell"></div><div data-cell-index="2" class="cell"></div><div data-cell-index="3" class="cell"></div><div data-cell-index="4" class="cell"></div><div data-cell-index="5" class="cell"></div><div data-cell-index="6" class="cell"></div><div data-cell-index="7" class="cell"></div><div data-cell-index="8" class="cell"></div></div><h2 class="game--status"></h2><button class="game--restart">Restart Game</button></div>';
 
 function TicTacToe(){
     const statusDisplay = document.querySelector('.game--status');
@@ -121,6 +122,142 @@ function TicTacToe(){
     document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
     document.querySelector('.game--restart').addEventListener('click', handleRestartGame);
 }
+// End of TicTacToe Code
+
+// Start of Flappy Bird Code
+const flappyBird = '<div id="game-container"><h1>Flappy Bird</h1><br><br><br><br><button class="game--restart" onclick="restartGame()">Restart</button><button class="game--restart" onmousedown="accelerate(-0.2)" onmouseup="accelerate(0.05)">Fly!</button></div>';
+
+var myGamePiece;
+var myObstacles = [];
+var myScore;
+var flappyBirdScore
+
+function startGame() {
+    myGamePiece = new component(30, 30, "#95AFBA", 10, 120);
+    myGamePiece.gravity = 0.05;
+    myScore = new component("30px", "Consolas", "black", 280, 40, "text");
+    myGameArea.start();
+}
+
+function restartGame() {
+    myGamePiece = {};
+    myScore = {};
+    clearInterval(myGameArea.interval)
+    myGameArea.clear();
+    myObstacles = [];
+    myGamePiece = new component(30, 30, "#95AFBA", 10, 120);
+    myGamePiece.gravity = 0.05;
+    myScore = new component("30px", "Consolas   ", "black", 280, 40, "text");
+    myGameArea.start();
+}
+
+var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext("2d");
+        document.getElementById("game-container").insertBefore(this.canvas, document.getElementById("game-container").childNodes[3]);
+        this.frameNo = 0;
+        this.interval = setInterval(updateGameArea, 15);
+        },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
+
+function component(width, height, color, x, y, type) {
+    this.type = type;
+    this.score = 0;
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;    
+    this.x = x;
+    this.y = y;
+    this.gravity = 0;
+    this.gravitySpeed = 0;
+    this.update = function() {
+        ctx = myGameArea.context;
+        if (this.type == "text") {
+            ctx.font = this.width + " " + this.height;
+            ctx.fillStyle = color;
+            ctx.fillText(this.text, this.x, this.y);
+        } else {
+            ctx.fillStyle = color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
+    }
+    this.newPos = function() {
+        this.gravitySpeed += this.gravity;
+        this.x += this.speedX;
+        this.y += this.speedY + this.gravitySpeed;
+        this.hitBottom();
+    }
+    this.hitBottom = function() {
+        var rockbottom = myGameArea.canvas.height - this.height;
+        if (this.y > rockbottom) {
+            this.y = rockbottom;
+            this.gravitySpeed = 0;
+        }
+    }
+    this.crashWith = function(otherobj) {
+        var myleft = this.x;
+        var myright = this.x + (this.width);
+        var mytop = this.y;
+        var mybottom = this.y + (this.height);
+        var otherleft = otherobj.x;
+        var otherright = otherobj.x + (otherobj.width);
+        var othertop = otherobj.y;
+        var otherbottom = otherobj.y + (otherobj.height);
+        var crash = true;
+        if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)) {
+            crash = false;
+        }
+        return crash;
+    }
+}
+
+function updateGameArea() {
+    var x, height, gap, minHeight, maxHeight, minGap, maxGap;
+    for (i = 0; i < myObstacles.length; i += 1) {
+        if (myGamePiece.crashWith(myObstacles[i])) {
+            flappyBirdScore = myGameArea.frameNo;
+            return;
+        } 
+    }
+    myGameArea.clear();
+    myGameArea.frameNo += 1;
+    if (myGameArea.frameNo == 1 || everyinterval(150)) {
+        x = myGameArea.canvas.width;
+        minHeight = 20;
+        maxHeight = 200;
+        height = Math.floor(Math.random()*(maxHeight-minHeight+1)+minHeight);
+        minGap = 50;
+        maxGap = 200;
+        gap = Math.floor(Math.random()*(maxGap-minGap+1)+minGap);
+        myObstacles.push(new component(10, height, "rgb(32, 109, 197)", x, 0));
+        myObstacles.push(new component(10, x - height - gap, "rgb(32, 109, 197)", x, height + gap));
+    }
+    for (i = 0; i < myObstacles.length; i += 1) {
+        myObstacles[i].x += -1;
+        myObstacles[i].update();
+    }
+    myScore.text="SCORE: " + myGameArea.frameNo;
+    myScore.update();
+    myGamePiece.newPos();
+    myGamePiece.update();
+}
+
+function everyinterval(n) {
+    if ((myGameArea.frameNo / n) % 1 == 0) {return true;}
+    return false;
+}
+
+function accelerate(n) {
+    myGamePiece.gravity = n;
+}
+// End of Flappy Bird Code
 
 function showGame(num){
     const main = document.getElementById('main');
@@ -129,5 +266,9 @@ function showGame(num){
     {
         main.innerHTML += ticTicToe;
         TicTacToe();
+    }
+    else if (num == 5){
+        main.innerHTML += flappyBird;
+        startGame();
     }
 }
